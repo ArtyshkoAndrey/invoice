56e6687051e7336bb1722ce83e05996a673244f4
Fulliton <artyshko.andrey@gmail.com>
2021-10-04 17:56:00 +0700

    seed sample

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Day;
 use App\Models\User;
+use App\Models\Sample;
+use App\Models\Resort;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -37,5 +40,22 @@ class DatabaseSeeder extends Seeder
       AirportTableSeed::class,
       ResortTableSeed::class,
     ]);
+    if (Sample::all()->count() === 0) {
+      for($i = 0; $i < 10; $i++) {
+        $sample = new Sample([
+          'name' => 'Tур на ' . 10 - $i - 1 . ' дней'
+        ]);
+        $sample->save();
+        for ($j = 0; $j < 10 - $i - 1; $j++) {
+          $day = new Day();
+          $day->order = $j + 1;
+          $day->free = rand(0, 1);
+          $day->resort()->associate(Resort::all()->random(1)->first()->id);
+          $day->model()->associate($sample);
+          $day->save();
+        }
+      }
+    }
+
   }
 }
