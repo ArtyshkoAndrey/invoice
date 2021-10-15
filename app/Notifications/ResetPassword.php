59545895858236a8e0f7ea15bd683507702a028e
Fulliton <artyshko.andrey@gmail.com>
2021-10-15 15:14:16 +0700

    lang notify

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -2,8 +2,8 @@
 
 namespace App\Notifications;
 
-use Illuminate\Auth\Notifications\ResetPassword as Notification;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Auth\Notifications\ResetPassword as Notification;
 
 class ResetPassword extends Notification
 {
@@ -11,13 +11,16 @@ class ResetPassword extends Notification
    * Build the mail representation of the notification.
    *
    * @param mixed $notifiable
+   *
    * @return MailMessage
    */
   public function toMail($notifiable): MailMessage
   {
     return (new MailMessage)
-      ->line('You are receiving this email because we received a password reset request for your account.')
-      ->action('Reset Password', url(config('app.url') . '/password/reset/' . $this->token) . '?email=' . urlencode($notifiable->email))
+      ->subject(__('Password reset in the service ') . config('app.name'))
+      ->greeting(__('Hello!'))
+      ->line(__('You are receiving this email because we received a password reset request for your account.'))
+      ->action(__('Reset Password'), url(config('app.url') . '/password/reset/' . $this->token) . '?email=' . urlencode($notifiable->email))
       ->line('If you did not request a password reset, no further action is required.');
   }
 }
